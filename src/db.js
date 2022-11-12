ccd46a1d16dc8679e8f596b3d14b8a877a3402a3
biteme <caro@Ata-MBP.fibertel.com.ar>
2022-11-12 18:12:55 -0300

    feat: update api

--- src/db.js
+++ src/db.js
@@ -13,6 +13,7 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
+
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, "/models"))
   .filter(
